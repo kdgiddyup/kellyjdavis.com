890a74907e6503d15edf637c3f9079f7029824d5
kjdgac <kelly.davis@gulfstream.com>
2018-10-30 12:33:07 -0400

    updates readme, adds favicon, analytics tag logic to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -51,7 +51,7 @@ const getURLparams = require("./libraries/getURLparams");
 /*------------- 
     Images 
 ---------------*/
-const favicon = require("./../img/favicon.ico");
+import favicon from "./../img/favicon.ico";
 
 /*---------
     HTML
@@ -61,5 +61,61 @@ import "./../index.html";
 $(() => {
   // after page load, do:
 
+  $("link[rel='shortcut icon']").attr("href", favicon);
+  // update favicon
+
+  /*********************************
+   * Configure analytics tags here *
+   *********************************/
+
+  const siteUrl = "";
+  // what will this site's URL be?
+  // No analytics tagging will be applied if this is left blank
+
+  const hotjarId = "";
+  // what is this site's Hotjar analytics ID?
+  // No hotjar tag will be applied if this is left blank
+
+  const googleId = "";
+  // what is this site's Google Analtyics ID?
+  // No Google Analytics tag will be applied if this is left blank
+
+  if (siteUrl) {
+    if (hotjarId) {
+      $("head").append(`<!-- Hotjar Tracking Code for ${siteUrl} -->
+  <script>
+  (function (h, o, t, j, a, r) {
+      h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+      h._hjSettings = { hjid: ${hotjarId}, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script'); r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+</script>`);
+    }
+    // if hotjar ID is given, add hotjar tag
+
+    if (googleId) {
+      $("head")
+        .append(`<!-- Global site tag (gtag.js) for ${siteUrl} - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=${
+        variants[variant].analytics.google
+      }"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+
+  gtag('config', '${variants[variant].analytics.google}');
+</script>`);
+    }
+    // if Google Analytics ID is given, add Google Analytics tag
+  }
+  // if analytics are configured, append them to the <head> tag
+
+  /******************************
+   * Site scripting starts here *
+   ******************************/
   $(".container").prepend('<div data-display="page title">Hello, world!</div>');
 }); // end doc ready function
