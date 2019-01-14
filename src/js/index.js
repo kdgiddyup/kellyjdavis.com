608f6b31feec358cc6106b512435a416f2845477
kjdgac <kelly.davis@gulfstream.com>
2019-01-14 09:07:42 -0500

    fixes googleId path in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -59,30 +59,30 @@ import favicon from "./../img/favicon.ico";
 import "./../index.html";
 
 $(() => {
-  // after page load, do:
+    // after page load, do:
 
-  $("link[rel='shortcut icon']").attr("href", favicon);
-  // update favicon
+    $("link[rel='shortcut icon']").attr("href", favicon);
+    // update favicon
 
-  /*********************************
-   * Configure analytics tags here *
-   *********************************/
+    /*********************************
+     * Configure analytics tags here *
+     *********************************/
 
-  const siteUrl = "";
-  // what will this site's URL be?
-  // No analytics tagging will be applied if this is left blank
+    const siteUrl = "";
+    // what will this site's URL be?
+    // No analytics tagging will be applied if this is left blank
 
-  const hotjarId = "";
-  // what is this site's Hotjar analytics ID?
-  // No hotjar tag will be applied if this is left blank
+    const hotjarId = "";
+    // what is this site's Hotjar analytics ID?
+    // No hotjar tag will be applied if this is left blank
 
-  const googleId = "";
-  // what is this site's Google Analtyics ID?
-  // No Google Analytics tag will be applied if this is left blank
+    const googleId = "";
+    // what is this site's Google Analtyics ID?
+    // No Google Analytics tag will be applied if this is left blank
 
-  if (siteUrl) {
-    if (hotjarId) {
-      $("head").append(`<!-- Hotjar Tracking Code for ${siteUrl} -->
+    if (siteUrl) {
+        if (hotjarId) {
+            $("head").append(`<!-- Hotjar Tracking Code for ${siteUrl} -->
   <script>
   (function (h, o, t, j, a, r) {
       h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
@@ -93,29 +93,29 @@ $(() => {
       a.appendChild(r);
   })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
 </script>`);
-    }
-    // if hotjar ID is given, add hotjar tag
+        }
+        // if hotjar ID is given, add hotjar tag
 
-    if (googleId) {
-      $("head")
-        .append(`<!-- Global site tag (gtag.js) for ${siteUrl} - Google Analytics -->
+        if (googleId) {
+            $("head")
+                .append(`<!-- Global site tag (gtag.js) for ${siteUrl} - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=${
-        variants[variant].analytics.google
-      }"></script>
+                    googleId
+                    }"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag() { dataLayer.push(arguments); }
   gtag('js', new Date());
 
-  gtag('config', '${variants[variant].analytics.google}');
+  gtag('config', '${googleId}');
 </script>`);
+        }
+        // if Google Analytics ID is given, add Google Analytics tag
     }
-    // if Google Analytics ID is given, add Google Analytics tag
-  }
-  // if analytics are configured, append them to the <head> tag
+    // if analytics are configured, append them to the <head> tag
 
-  /******************************
-   * Site scripting starts here *
-   ******************************/
-  $(".container").prepend('<div data-display="page title">Hello, world!</div>');
+    /******************************
+     * Site scripting starts here *
+     ******************************/
+    $(".container").prepend('<div data-display="page title">Hello, world!</div>');
 }); // end doc ready function
